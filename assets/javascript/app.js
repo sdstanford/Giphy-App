d7bbdccfdfdf90e2b57e355b352f02f6fb86f69f
Shannon Doepke <shannondoepke@Shannons-Air.lan>
2018-03-29 18:22:41 -0400

    update JS with working gif buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,13 +10,45 @@ $("#submit-button").on("click", function(event) {
     showButton.text(newShow);
     $("#button-div").append(showButton);
 
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + newShow + "&api_key=bBlegFujhJCYdMATQpNX51744gjcVYd0&limit=10";
+    $("#" + newShow).on("click", function(event) {
+        event.preventDefault();
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-});
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + newShow + "&api_key=bBlegFujhJCYdMATQpNX51744gjcVYd0&limit=10";
 
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function(response) {
+            var results = response.data;
+
+            console.log(results);
+
+        for(i = 0; i < results.length; i++){
+
+            // Creating a div with the class "item"
+            var gifDiv = $("<div class='item'>");
+
+            // Storing the result item's rating
+            var rating = results[i].rating;
+
+            // Creating a paragraph tag with the result item's rating
+            var p = $("<p>").text("Rating: " + rating);
+
+            // Creating an image tag
+            var tvGif = $("<img>");
+
+            // Giving the image tag an src attribute of a proprty pulled off the
+            // result item
+            tvGif.attr("src", results[i].images.fixed_height.url);
+
+            // Appending the paragraph and personImage we created to the "gifDiv" div we created
+            gifDiv.append(p);
+            gifDiv.append(tvGif);
+
+            // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+            $("#gif-div").prepend(gifDiv);
+        }
+        });
+    });
 });
